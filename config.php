e8a6b9d868060a2d03236811fe48738d46906b07
brent.huo <qixing.huo@gmail.com>
2012-10-29 11:24:57 +0800

    sina weibo api demo

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+header('Content-Type: text/html; charset=UTF-8');
+
+define( "WB_AKEY" , '1097955374' );
+define( "WB_SKEY" , '065715409686b24da87e85ba68bfc352' );
+define( "WB_CALLBACK_URL" , 'http://sinagame.zancol.com/callback.php' );
--- index.php
+++ index.php
@@ -1,144 +1,35 @@
 <?php
+session_start();
 
-/**
- * 
- * 微游戏API演示
- * 
- * 
- * @author ixqbar@gmail.com 
- * http://xingqiba.sinaapp.com
- * 
- * 需要注意api.class.php 139行 修改使其在32bit机器可以运行
- * 
- * 
- */
+include_once( 'config.php' );
+include_once( 'saetv2.ex.class.php' );
 
-/**
- * 配置
- */
-$config = array(
-    'app_key'    => '1097955374',
-    'app_secret' => '065715409686b24da87e85ba68bfc352',
-    'app_url'    => 'http://game.weibo.com/zancol_hitom',
-    'app_name'   => 'zancol_hitom',
-    'game_url'   => 'http://sinagame.zancol.com/',
-    'version'    => uniqid()
-);
+$o = new SaeTOAuthV2( WB_AKEY , WB_SKEY );
 
-/**
- * 
- */
+$code_url = $o->getAuthorizeURL( WB_CALLBACK_URL );
 
-include_once './sdk/api.class.php';
-
-class Weiyouxi_Api extends WeiyouxiClient {
-
-    public function setSessionKey($sessionKey) {
-        $this->sessionKey = $sessionKey;
-    }
-
-    public function setSignature($signature) {
-        $this->signature = $signature;
-    }
-
-}
-
-/**
- * 
- * 获取用户token信息并保存，以方便逻辑中请求API时使用
- * 
- */
-$player_token = array(
-    'session_key' => isset($_GET['wyx_session_key']) ? $_GET['wyx_session_key'] : '',
-    'signature'   => isset($_GET['wyx_signature']) ? $_GET['wyx_signature'] : ''
-);
-
-$weiyouxi_handle = new Weiyouxi_Api($config['app_key'], $config['app_secret']);
-$player_uid  = $weiyouxi_handle->getUserId();
-$player_info = $weiyouxi_handle->get('user/show');
-if (empty($player_info) || isset($player_info['error'])) {
-    die("登录失败");
-}
-
-/**
- * 存储用户信息
- * @TODO
- */
-
-
-/**
- * 打印用户应用好友
- * 获取安装本应用的互粉好友ID，注意API返回的某些玩家并不在游戏内(游戏自身维护期间，好友点击游戏平台已经认为他安装了游戏，但维护是这个用户并没有进入游戏DB)，
- * 所以使用该API自动添加为自己游戏邻居的需要判断该好友是否已经在游戏内
- */
-$player_app_friend_ids = $weiyouxi_handle->get('user/app_friend_ids');
-//var_dump($player_app_friend_ids);
-
-
-/**
- * 
- * 发送成就
-$params = array(
-    'achv_id' => 1,
-);
-$weiyouxi_handle = new Weiyouxi_Api($config['app_key'], $sns_config['app_secret']);
-$weiyouxi_handle->setSessionKey($player_token['session_key']);
-$weiyouxi_handle->setSignature($player_token['signature']);
-$post_response = $weiyouxi_handle->post('achievements/set', $params);
-*/
-
-/**
- * 发送排行
- * 注意目前平台只接受大于上次提交的排行分数
-$params = array(
-    'rank_id' => 1,
-    'value'   => 100,
-);
-
-$weiyouxi_handle = new Weiyouxi_Api($config['app_key'], $config['app_secret']);
-$weiyouxi_handle->setSessionKey($token['session_key']);
-$weiyouxi_handle->setSignature($token['signature']);
-$post_response = $weiyouxi_handle->post('leaderboards/set', $params);
-*/
-
-
-/**
- * 提示:
- * JS发布后最好压缩到一个JS文件
- */
 ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>新浪微博PHP SDK V2版 Demo - Powered by Sina App Engine</title>
+</head>
 
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <title>微博游戏API演示</title>
-    </head>
-    <body>
-        <div><a href="javascript:Test.inviteFriend();">邀请好友</a></div>
-        <div><a href="javascript:Test.sendGiftForOne({});">给某人发送礼物</a></div>
-        <div><a href="javascript:Test.createFriend('2271093597');">关注好友</a></div>
-        <div><a href="javascript:Test.postWall({});">发送新鲜事(又名贴墙)</a></div>
-        <!--浮动层标记-->
-        <div id="test-win" style="display:none;z-index:100;height:100%;width:100%;position:fixed;top:0px;left:0px;color:#fff;"></div>
-    </body>
+<body>
+	<p>新浪微博PHP SDK由新浪SAE团队开发和维护，已集成在新浪SAE平台，SAE团队会负责对其进行维护和更新，平台开发者无需自行下载更新即可直接调用最新SDK，使用微博最新API。</p>
+	<p>本DEMO演示了PHP SDK的授权及接口调用方法，开发者可以在此基础上进行灵活多样的应用开发。</p>
+	<hr />
+	<p>什么是新浪SAE？</p>
+	<p>新浪SAE，全称Sina App Engine( <a href="http://sae.sina.com.cn" target="_blank">http://sae.sina.com.cn</a> )，是新浪研发中心推出的国内首个公有云计算平台。</p>
+	<p>SAE选择在国内流行最广的Web开发语言PHP作为首选的支持语言，并即将提供JAVA、Python语言支持。</p>
+	<p>应用开发者可以通过SVN、SDK或者在线代码编辑器进行开发、部署、调试，并可以通过应用成员管理、SVN代码管理方便地进行团队协作开发。</p>
+	<p>SAE为开发者提供MySQL、Storage、Memcache、KVDB、XHProf、定时任务、异步任务队列、计数器服务、分词服务、全文检索服务等众多服务，用户无需关心运维和一些高难度技术即可快速开发，极大提高开发效率。</p>
+	<p>SAE采用“所付即所用，所付仅所用”的计费理念，通过日志和统计中心精确的计算每个应用的资源消耗，进行精确计费。并且SAE会对通过实名认证的开发者赠送免费额度，使开发者可以零成本创业。</p>
+	<p>总而言之，SAE就是简单高效的分布式web服务开发、运行平台。</p>
+	<p>更多SAE相关内容，请点击 <a href="http://sae.sina.com.cn/?m=devcenter" target="_blank">http://sae.sina.com.cn/?m=devcenter</a> 。</p>
+	<!-- 授权按钮 -->
+    <p><a href="<?=$code_url?>"><img src="weibo_login.png" title="点击进入授权页面" alt="点击进入授权页面" border="0" /></a></p>
+
+</body>
 </html>
-<script src=" http://tjs.sjs.sinajs.cn/open/api/js/wb.js?appkey=<?php echo $config['app_key'];?>" type="text/javascript"></script>
-<script type="text/javascript" src="http://game.weibo.com/static/js/v0.3/wyx.connect.js.php"></script>
-<script type="text/javascript" src="http://lib.sinaapp.com/js/jquery/1.7.2/jquery.min.js"></script>
-<script type="text/javascript" src="http://lib.sinaapp.com/js/swfobject/2.2/swfobject.js"></script>
-<script type="text/javascript" src="<?php echo $config['game_url']; ?>js/test.util.js?v=<?php echo $config['version']; ?>"></script>
-<script type="text/javascript" src="<?php echo $config['game_url']; ?>js/test.common.js?v=<?php echo $config['version']; ?>"></script>
-<script type="text/javascript" src="<?php echo $config['game_url']; ?>js/test.data.js?v=<?php echo $config['version']; ?>"></script>
-<script type="text/javascript" src="<?php echo $config['game_url']; ?>js/test.language.js?v=<?php echo $config['version']; ?>"></script>
-<script type="text/javascript" src="<?php echo $config['game_url']; ?>js/test.template.js?v=<?php echo $config['version']; ?>"></script>
-<script type="text/javascript">
-    $(function(){
-        Test.init({
-            uid     : '<?php echo $player_uid;?>',
-            appKey  : '<?php echo $config['app_key'];?>',
-            appURL  : '<?php echo $config['app_url'];?>',
-            gameURL : '<?php echo $config['game_url'];?>'
-        });
-    });
-</script>
