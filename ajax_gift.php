c61dfb5be19df0e357ea23773eb068679891bda8
brent.huo <qixing.huo@gmail.com>
2012-10-29 14:36:11 +0800

    delete useless

--- ajax_gift.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-/**
- * 
- */
-
-$data = array('code' => 0, 'data' => '');
-echo '<script type="text/javascript">var frame = window.parent;while(frame != frame.parent && frame.name != "app_frame") {frame=frame.parent;}frame.Test.frameCallBack(' . json_encode($data) .');</script>';
-    
--- ajax_wall.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-/**
- * 游戏逻辑
- */
-
-
-echo json_encode(array('code' => 0, 'data' => ''));
